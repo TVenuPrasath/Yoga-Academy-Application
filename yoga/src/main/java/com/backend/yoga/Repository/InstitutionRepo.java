f87fcd6b9f1965a8348246ee75f9459ccbb40040
TVenuPrasath <124986744+TVenuPrasath@users.noreply.github.com>
2025-01-11 17:13:06 +0530

    Delete yoga directory

--- yoga/src/main/java/com/backend/yoga/Repository/InstitutionRepo.java
+++
@@ -1,8 +0,0 @@
-package com.backend.yoga.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.backend.yoga.Model.InstitutionModel;
-
-public interface InstitutionRepo extends JpaRepository<InstitutionModel,Integer>{
-}
--- yoga/src/main/java/com/backend/yoga/Repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.backend.yoga.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.backend.yoga.Model.UserModel;
-
-public interface UserRepo  extends JpaRepository<UserModel,Integer>{
-    
-}
